95588e32722d62a44b4966b86b255c52016415f8
Tuan Nguyen <38831897+justmangoou@users.noreply.github.com>
2026-06-26 21:07:20 +0700

    chore: add OpenRGB

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 set -Ux DOCKER_HOST "unix:///run/user/"(id -u)"/podman/podman.sock"
 
+starship init fish | source
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
