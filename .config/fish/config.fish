54b25ea5e69c3069a3e5d37c5adf0b34d4f4cd2d
Tuan Nguyen <38831897+justmangoou@users.noreply.github.com>
2026-07-25 21:44:20 +0700

    chore: config update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 set -Ux DOCKER_HOST "unix:///run/user/"(id -u)"/podman/podman.sock"
+set -Ux fish_user_paths $HOME/.cargo/bin $fish_user_paths
 
 starship init fish | source
 
@@ -9,3 +10,10 @@ starship init fish | source
 #function fish_greeting
 #    # smth smth
 #end
+
+# pnpm
+set -gx PNPM_HOME "/home/justmango/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
